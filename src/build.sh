6413b1f08730003340f41cf4ec84da748d10d8e8
ordinary-slim <mehdi.slimani.elmaddarsi@gmail.com>
2025-12-19 01:28:21 +0100

    cmake
    instead of build.sh

--- src/build.sh
+++
@@ -1,7 +0,0 @@
-wayland-scanner private-code \
-  < /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml \
-  > xdg-shell-protocol.c
-wayland-scanner client-header \
-  < /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml \
-  > xdg-shell-client-protocol.h
-gcc -o main main.cpp alloc_shmpool.cpp xdg-shell-protocol.c -lwayland-client -lrt
